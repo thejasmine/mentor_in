bad68cf90eefe06df14f1aeecf0db4a47849ec63
Jasmine Huang <tinjasminee@gmail.com>
2020-08-14 21:59:50 +0800

    add show

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,4 +4,29 @@ skip_before_action :verify_authenticity_token
     @events = Event.all
     render json: @events, include: 'user'
   end
+
+  def show
+    @event = Event.find(params[:id])
+    render json: @event, include: ['user','appointments']
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+
+  private
+
+  def event_params
+     params.require(:event).permit(:name, :description,:category,:level,:link,:start_time, :end_time,:capacity)
+  end
+
+  def render_error
+    render json: {error: @event.errors.full_messaages}
+  end
 end
