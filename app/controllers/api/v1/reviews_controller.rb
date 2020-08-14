2ef24c42c9fe0fea1eb0975dfc8a2a36ce24772e
Jasmine Huang <tinjasminee@gmail.com>
2020-08-14 22:19:48 +0800

    add review controller

---
+++ app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @appointment = Appointment.find(params[:appointment_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @appointment = Appointment.find(params[:appointment_id])
+    @review.appointment_id = @appointment.id
+    if @review.save
+      render json: @review
+      redirect_to appointments_path
+    else
+      raise
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content,:rating)
+  end
+end
